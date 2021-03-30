b7984b5acee084fa8f08411ae57d61e215138e24
InkoHX <me@inkohx.dev>
2021-03-30 17:30:59 +0900

    major update

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Usage
 
 ```txt
-https://og-generator.now.sh/?title=%E6%88%91%E3%80%85%E3%81%AF%E3%81%AA%E3%81%9C%E8%AA%95%E7%94%9F%E3%81%97%E3%81%9F%E3%81%AE%E3%81%8B&date=2021-03-02T17:32:34.000Z
+https://og-generator.now.sh/api/title/date (ISO format)/og.png
 ```
 
-![Screenshot](https://og-generator.now.sh/?title=%E6%88%91%E3%80%85%E3%81%AF%E3%81%AA%E3%81%9C%E8%AA%95%E7%94%9F%E3%81%97%E3%81%9F%E3%81%AE%E3%81%8B&date=2021-03-02T17:32:34.000Z)
+![Screenshot](https://og-generator.now.sh/api/%E6%88%91%E3%80%85%E3%81%AF%E3%81%AA%E3%81%9C%E8%AA%95%E7%94%9F%E3%81%97%E3%81%9F%E3%81%AE%E3%81%8B/2021-03-02T17:32:34.000Z/og.png)
--- api/[title]/[date]/og.png.ts
+++ api/[title]/[date]/og.png.ts
@@ -4,17 +4,14 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import path from 'path'
 import { promises as fs } from 'fs'
 
-const getLaunchOptions = async (): Promise<LaunchOptions> => process.env.IS_PRODUCUTION
+const getLaunchOptions = async (): Promise<LaunchOptions> => process.env.AWS_REGION
   ? {
-    executablePath: await chromeLambda.executablePath,
-    headless: chromeLambda.headless,
-    args: chromeLambda.args
+    executablePath: await chromeLambda.executablePath
   } : {
-    executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-    headless: true
+    executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
   }
 
-const getScreenshot = async (title: string, date: Date, platform = 'InkoHX Blog'): Promise<Buffer> => {
+const getScreenshot = async (title: string, date: Date): Promise<string | void | Buffer> => {
   const dateTime = new Intl.DateTimeFormat('en-US', {
     dateStyle: 'medium',
     timeStyle: 'short'
@@ -25,8 +22,7 @@ const getScreenshot = async (title: string, date: Date, platform = 'InkoHX Blog'
   await page.setViewport({ width: 1280, height: 680 })
   await page.setContent((await fs.readFile(path.join(__dirname, '_static/template.html'), 'utf-8'))
     .replace('(TITLE_TEXT)', title)
-    .replace('(DATE)', dateTime.format(date))
-    .replace('(PLATFORM)', platform))
+    .replace('(DATE)', dateTime.format(date)))
 
   return page.screenshot({ type: 'png' })
 }
@@ -34,13 +30,12 @@ const getScreenshot = async (title: string, date: Date, platform = 'InkoHX Blog'
 // eslint-disable-next-line
 export default async function (request: VercelRequest, response: VercelResponse) {
   try {
-    const { title, date, platform } = request.query
+    const { title, date } = request.query
 
     if (typeof title !== 'string') return response.status(400).send({ message: 'Title is required' })
     if (typeof date !== 'string' || Number.isNaN(Date.parse(date))) return response.status(400).send({ message: 'The data format is incorrect.' })
-    if (Array.isArray(platform)) return response.status(400).send({ message: 'The platform is incorrect.' })
 
-    const screenshot = await getScreenshot(title, new Date(date), platform)
+    const screenshot = await getScreenshot(title, new Date(date))
 
     response.setHeader('Content-Type', 'image/png')
     response.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
